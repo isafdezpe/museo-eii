968e7c8df603de9cd40cb90962806e82f06021e2
Isabel Fernández Pérez <isafdezpe98@gmail.com>
2021-10-01 14:16:55 +0200

    css mejorado

--- src/app/comp-details/comp-details.component.ts
+++ src/app/comp-details/comp-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Cpu, CpuDevices } from '../cpu';
 import { CpusService } from '../cpus.service';
+import { PeriodService } from '../period.service';
 
 @Component({
   selector: 'app-comp-details',
@@ -11,18 +12,30 @@ import { CpusService } from '../cpus.service';
 export class CompDetailsComponent implements OnInit {
 
   comp: Cpu | undefined;
+  compsFromPeriod: Cpu[] = [];
+  periodName: string = '';
 
-  constructor(private route: ActivatedRoute,private cpuService: CpusService) { }
+  constructor(private route: ActivatedRoute,private cpuService: CpusService, private periodService: PeriodService) { }
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
 	  const idFromRoute = Number(routeParams.get('id'));
 
     this.getComp(idFromRoute);
+    this.getCompsFromPeriod();
+    this.getPeriodName();
   }
 
   getComp(id: number) {
     this.comp = this.cpuService.getCpu(id);
+  }
+
+  getCompsFromPeriod() {
+    this.compsFromPeriod = this.cpuService.getCpusFromPeriod(this.comp.periodId);
+  }
+
+  getPeriodName() {
+    this.periodName = this.periodService.getPeriod(this.comp.periodId).name;
   }
 
   isPortable() {
@@ -31,5 +44,9 @@ export class CompDetailsComponent implements OnInit {
 
   isDesktop() {
     return this.comp.devices.includes(CpuDevices.desktop);
+  }
+
+  refresh(): void {
+    window.location.reload();
   }
 }
