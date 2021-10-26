fada7b871898bfd1f3f52539bca45f87d9311aab
Isabel Fernández Pérez <isafdezpe98@gmail.com>
2021-10-26 12:27:26 +0200

    validacion del formulario añadir periodo

--- museo-eii-admin/src/app/form-add/form-add.component.ts
+++ museo-eii-admin/src/app/form-add/form-add.component.ts
@@ -14,10 +14,17 @@ export class FormAddComponent implements OnInit {
   }
 
   goToAddComp() {
-    //save period 
-    //get id
-    let periodId: number = 1;
-    this.router.navigateByUrl('/addComp/' + periodId);
+    var name = document.getElementById("pname");
+    var desc = document.getElementById("pdesc");
+    var trivia = document.getElementById("ptrivia");
+    var events = document.getElementById("pevents");
+    var isValid = name.innerText.length > 0 && desc.innerText.length > 0 && trivia.innerText.length > 0 && events.innerText.length > 0;
+    if (isValid) {
+      //save period 
+      //get id
+      let periodId: number = 1;
+      this.router.navigateByUrl('/addComp/' + periodId);
+    }
   }
 
 }
