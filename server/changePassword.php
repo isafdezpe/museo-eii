93a3d1dc7a3c4780f77a12e4712f39586932942f
Isabel Fernández Pérez <isafdezpe98@gmail.com>
2022-06-01 15:34:18 +0200

    script cambio contraseña

--- server/changePassword.php
+++ server/changePassword.php
@@ -1,30 +1,17 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:4200");
-if (empty($_GET["email"])) {
-    exit("No hay usuario");
+$user = trim(readline("Introduzca el usuario: "));
+$pass = trim(readline("Introduzca una nueva contraseña: "));
+$pass2 = trim(readline("Repita la contraseña: "));
+if ($pass == $pass2 && $pass != "") {
+	try {
+		$bd = include_once "bd.php";
+		$sentence = $bd->prepare("UPDATE administrator SET pwd = AES_ENCRYPT(?, 'museum_key_crypt') where email = ?");
+		$res = $sentence->execute([$pass, $user]);
+		echo "Contraseña actualizada con éxito.\n";
+	} catch (PDOException $e) {
+		echo "Ha habido un error al actualizar la contraseña.\n";
+	}
+} else {
+	echo "Las contraseñas no coinciden.\n";
 }
-$email = $_GET["email"];
-try {
-    $bd = include_once "bd.php";
-    $sentence = $bd->prepare("select * from administrator where pwd = ?");
-    $sentence->execute([$email]);
-    $res = $sentence->fetchObject();
-    if ($res) {
-        $token = md5($emailId).rand(10,9999);
-        $expFormat = mktime(
-        date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y")
-        );
-        $expDate = date("Y-m-d H:i:s",$expFormat);
-        $update = $bd->prepare("update administrator set pwd = ?, reset_link_token = ?, expDate = ? where email = ?");
-        $update->execute([$password, $token, $expDate, $email]);
-        $link = "<a href='http://localhost:80/changePassword.php?key=".$email."&amp;token=".$token."'>Click aquí para cambiar la contraseña</a>";
-        
-    } else {
-        http_response_code(400);
-        exit("Dirección de email inválida");
-    }
-} catch (PDOException $e) {
-    error_log('PDOException - ' . $e->getMessage(), 0);
-    http_response_code(500);
-    exit("Error al conectar con la base de datos");
-}
+?>
