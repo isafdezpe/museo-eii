56c5f893d9d71e46a88bf11d8d46d48f4f498802
Isabel Fernández Pérez <isafdezpe98@gmail.com>
2022-06-14 13:51:34 +0200

    pruebas museo terminadas

---
+++ museo-eii/src/app/comp.spec.ts
@@ -0,0 +1,45 @@
+import { CompDevices, CompTypes, Cpu, GenericComp, MemoryUnits, PowerUnits, PriceUnits, SpeedUnits } from "./comp";
+
+
+describe('GenericComp', () => {
+  it('should create an instance', () => {
+    expect(new GenericComp('Intel 4004', 'Intel', 'Desarrollado para calculadoras de la marca Busicom, se instaló en máquinas de Pinball y fue candidato a instalarse en la nave espacial Pioneer 10. Existe una réplica funcional de 41x58cm en el Intel Museum de California.',
+        1971, 1981, 1, 1800, PriceUnits.dolar, [CompDevices.desktop, CompDevices.portable], ['4004-1.jpeg', '4004-2.jpeg', '4004-3.jpeg'], "Busicom 141PF", "Busicom 141PF.jpeg", CompTypes.generic, 1)).toBeTruthy();
+  });
+
+  it('should set devices', () => {
+    let comp = new GenericComp('Intel 4004', 'Intel', 'Desarrollado para calculadoras de la marca Busicom, se instaló en máquinas de Pinball y fue candidato a instalarse en la nave espacial Pioneer 10. Existe una réplica funcional de 41x58cm en el Intel Museum de California.',
+        1971, 1981, 1, 1800, PriceUnits.dolar, [CompDevices.desktop, CompDevices.portable], ['4004-1.jpeg', '4004-2.jpeg', '4004-3.jpeg'], "Busicom 141PF", "Busicom 141PF.jpeg", CompTypes.generic, 1);
+    comp.setDevices([CompDevices.desktop]);
+    expect(comp.component_devices).toEqual("DESKTOP");
+  });
+
+  it('should check if component is equal', () => {
+    let comp = new GenericComp('Intel 4004', 'Intel', 'Desarrollado para calculadoras de la marca Busicom, se instaló en máquinas de Pinball y fue candidato a instalarse en la nave espacial Pioneer 10. Existe una réplica funcional de 41x58cm en el Intel Museum de California.',
+        1971, 1981, 1, 1800, PriceUnits.dolar, [CompDevices.desktop, CompDevices.portable], ['4004-1.jpeg', '4004-2.jpeg', '4004-3.jpeg'], "Busicom 141PF", "Busicom 141PF.jpeg", CompTypes.generic, 1);
+    expect(comp.equals(new GenericComp('Intel 4004', 'Intel', 'Desarrollado para calculadoras de la marca Busicom, se instaló en máquinas de Pinball y fue candidato a instalarse en la nave espacial Pioneer 10. Existe una réplica funcional de 41x58cm en el Intel Museum de California.',
+        1971, 1981, 1, 1800, PriceUnits.dolar, [CompDevices.desktop, CompDevices.portable], ['4004-1.jpeg', '4004-2.jpeg', '4004-3.jpeg'], "Busicom 141PF", "Busicom 141PF.jpeg", CompTypes.generic, 1))).toBeTruthy();
+    expect(comp.equals(new GenericComp('Intel 8008', 'Intel', 'Sucesor de 8 bits del 4004. Creado para el ordenador DataPoint 2200, pero no usado por su bajo rendimiento. Usado en terminales, embotelladoras, robots y computadoras simples. Tenía un juego de instrucciones propio.',
+        1972, 1983, 1, 120, PriceUnits.dolar, [CompDevices.desktop], ['8008-1.jpeg', '8008-2.jpeg', '8008-3.jpeg'], "Datapoint 2200", "Datapoint 2200.jpeg", CompTypes.generic, 2))).toBeFalsy();
+  });
+});
+
+describe('Cpu', () => {
+    it('should create an instance', () => {
+        expect(new Cpu('Intel 4004', 'Intel', 'Desarrollado para calculadoras de la marca Busicom, se instaló en máquinas de Pinball y fue candidato a instalarse en la nave espacial Pioneer 10. Existe una réplica funcional de 41x58cm en el Intel Museum de California.',
+            1971, 1981, 1, 1800, PriceUnits.dolar, [CompDevices.desktop, CompDevices.portable], ['4004-1.jpeg', '4004-2.jpeg', '4004-3.jpeg'], "Busicom 141PF", "Busicom 141PF.jpeg", 
+            4, MemoryUnits.kilobyte, 640, MemoryUnits.byte, 740, SpeedUnits.kiloherz, 0.45, PowerUnits.watios, 4, MemoryUnits.bit, 10000, 0.03, 2300, 1)).toBeTruthy();
+    });
+
+    it('should check if cpu is equal', () => {
+        let cpu = new Cpu('Intel 4004', 'Intel', 'Desarrollado para calculadoras de la marca Busicom, se instaló en máquinas de Pinball y fue candidato a instalarse en la nave espacial Pioneer 10. Existe una réplica funcional de 41x58cm en el Intel Museum de California.',
+            1971, 1981, 1, 1800, PriceUnits.dolar, [CompDevices.desktop, CompDevices.portable], ['4004-1.jpeg', '4004-2.jpeg', '4004-3.jpeg'], "Busicom 141PF", "Busicom 141PF.jpeg", 
+            4, MemoryUnits.kilobyte, 640, MemoryUnits.byte, 740, SpeedUnits.kiloherz, 0.45, PowerUnits.watios, 4, MemoryUnits.bit, 10000, 0.03, 2300, 1);
+        expect(cpu.equals(new Cpu('Intel 4004', 'Intel', 'Desarrollado para calculadoras de la marca Busicom, se instaló en máquinas de Pinball y fue candidato a instalarse en la nave espacial Pioneer 10. Existe una réplica funcional de 41x58cm en el Intel Museum de California.',
+            1971, 1981, 1, 1800, PriceUnits.dolar, [CompDevices.desktop, CompDevices.portable], ['4004-1.jpeg', '4004-2.jpeg', '4004-3.jpeg'], "Busicom 141PF", "Busicom 141PF.jpeg", 
+            4, MemoryUnits.kilobyte, 640, MemoryUnits.byte, 740, SpeedUnits.kiloherz, 0.45, PowerUnits.watios, 4, MemoryUnits.bit, 10000, 0.03, 2300, 1))).toBeTruthy();
+        expect(cpu.equals(new Cpu('Intel 4040', 'Intel', 'Uno de los dos sucesores del 4004 junto con el 8008, igualmente de 4 bits, pero que incorporaba nuevas instrucciones, registros y soporte para interrupciones hardware.',
+            1974, 1981, 1, 200, PriceUnits.dolar, [CompDevices.desktop, CompDevices.portable], ['4040-1.jpeg', '4040-2.jpeg', '4040-3.jpeg'], "Gottlieb Flying Carpet", "Gottlieb Flying Carpet.jpeg",
+            8, MemoryUnits.kilobyte, 640, MemoryUnits.byte, 740, SpeedUnits.kiloherz,  0.6, PowerUnits.watios, 4, MemoryUnits.bit, 10000, 0.03, 3000, 3))).toBeFalsy();
+    });
+});
--- museo-eii/src/app/period/period.component.spec.ts
+++ museo-eii/src/app/period/period.component.spec.ts
@@ -1,7 +1,9 @@
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { Observable, of } from 'rxjs';
 import { createTranslateLoader } from '../app.module';
 import { ComponentService } from '../cpus.service';
 import { ComponentMock } from '../mock-cpus';
@@ -43,13 +45,15 @@ describe('PeriodComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should get the actual period', () => {
-  //   let actual = component.period;
-  //   expect(actual.period_name).toEqual('CPUs pre-x86');
-  // });
+  it('should get the actual period', () => {
+    component.getPeriod(1);
+    let actual = component.period;
+    expect(actual.period_name).toEqual('CPUs pre-x86');
+  });
 
-  // it('should get next and previous period', () => {
-  //   expect(component.previousPeriod).toBeUndefined();
-  //   expect(component.nextPeriod.period_name).toEqual('8086 y 8088');
-  // });
+  it('should get next and previous period', () => {
+    component.getPeriod(1);
+    expect(component.previousPeriod).toBeUndefined();
+    expect(component.nextPeriod.period_name).toEqual('8086 y 8088');
+  });
 });
