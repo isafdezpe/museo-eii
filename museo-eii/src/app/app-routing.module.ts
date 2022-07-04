5a5e9322e89e5e7a05eb364c2c5db6334810fbd9
Isabel Fernández Pérez <isafdezpe98@gmail.com>
2022-07-04 16:28:53 +0200

    correccion rutas

--- museo-eii/src/app/app-routing.module.ts
+++ museo-eii/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload', useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
